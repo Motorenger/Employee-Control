c69d53fef7e659268da6c7ad3cb5af87865b79c6
Motorenger <90602764+Motorenger@users.noreply.github.com>
2023-02-28 23:56:57 +0200

    fixes

--- app/routers/users.py
+++ app/routers/users.py
@@ -16,7 +16,7 @@ router = APIRouter(
 
 
 @router.get("/", response_model=Page[User])
-async def users_list(params: Params = Depends(), db: Database = Depends(get_db)):
+async def users_list(params: Params = Depends(), db: Database = Depends(get_db)) -> Page:
     user_service = UserService(db)
 
     users = await user_service.get_users()
@@ -24,16 +24,15 @@ async def users_list(params: Params = Depends(), db: Database = Depends(get_db))
 
 
 @router.post("/create", response_model=User)
-async def users_create(user: UserCreate, db: Database = Depends(get_db)):
+async def users_create(user: UserCreate, db: Database = Depends(get_db)) -> User:
     user_service = UserService(db)
 
-    user = user.dict()
     user = await user_service.create_user(user)
     return user
 
 
 @router.get("/{user_id}", response_model=User)
-async def users_retrieve(user_id: int, db: Database = Depends(get_db)):
+async def users_retrieve(user_id: int, db: Database = Depends(get_db)) -> User:
     user_service = UserService(db)
 
     user = await user_service.retrieve_user(user_id)
@@ -41,17 +40,16 @@ async def users_retrieve(user_id: int, db: Database = Depends(get_db)):
 
 
 @router.put("/{user_id}", response_model=User)
-async def users_update(user_id: int, user_data: UserUpdate, db: Database = Depends(get_db)):
+async def users_update(user_id: int, user_data: UserUpdate, db: Database = Depends(get_db)) -> User:
     user_service = UserService(db)
 
-    data = user_data.dict()
     user = await user_service.update_user(user_id, data)
     return user
 
 
 @router.delete("/{user_id}",)
-async def users_delete(user_id: int, db: Database = Depends(get_db)):
+async def users_delete(user_id: int, db: Database = Depends(get_db)) -> str:
     user_service = UserService(db)
 
-    await user_service.delete_user(user_id)
-    return "User deleted successfully"
+    res = await user_service.delete_user(user_id)
+    return res
--- app/services/logic.py
+++ app/services/logic.py
@@ -2,65 +2,68 @@ from datetime import datetime
 
 from fastapi import HTTPException
 
+from db.models import users
 from utils.hashing import get_password_hash
+from schemas.user_schemas import UserCreate, User, UserUpdate
 
 
 class UserService:
     def __init__(self, db):
         self.db = db
+        self.users = users
 
-    async def check_for_existing(self, user_id: int = None, email: str = None):
+    async def check_for_existing(self, user_id: int = None, email: str = None) -> bool:
         if user_id:
-            query = "SELECT * FROM users WHERE id = :id"
-            user = await self.db.fetch_one(query, values={"id": user_id})
+            query = self.users.select().where(self.users.c.id == user_id)
+            user = await self.db.fetch_one(query)
+
             return bool(user)
         elif email:
-            query = "SELECT * FROM users WHERE email = :email"
-            user = await self.db.fetch_one(query, values={"email": email})
+            query = self.users.select().where(self.users.c.email == email)
+            user = await self.db.fetch_one(query)
             return bool(user)
 
     async def get_users(self) -> dict:
-        query = "SELECT * FROM users"
+        query = self.users.select()
         users = await self.db.fetch_all(query)
         return users
 
-    async def retrieve_user(self, user_id: int) -> dict:
-        query = "SELECT * FROM users WHERE id = :id"
-        user = await self.db.fetch_one(query, values={"id": user_id})
+    async def retrieve_user(self, user_id: int) -> User:
+        query = self.users.select().where(self.users.c.id == user_id)
+        user = await self.db.fetch_one(query)
         if user is None:
             raise HTTPException(status_code=404, detail="User not found")
-        return user
+        return User(**user)
 
-    async def create_user(self, user: dict) -> dict:
-        if self.check_for_existing(email=user["email"]):
+    async def create_user(self, user: UserCreate) -> User:
+        if await self.check_for_existing(email=user.email):
             raise HTTPException(status_code=400, detail="User is not unique.")
 
+        user = user.dict()
         user["password"] = get_password_hash(user["password"])
         user["is_active"] = False
         user["date_joined"] = datetime.now()
-        query = """INSERT INTO users(email, username, password, is_active, date_joined)
-                    VALUES (:email, :username, :password, :is_active, :date_joined)
-                """
+        query = self.users.insert()
         await self.db.execute(query=query, values=user)
 
-        users = await self.db.fetch_all(query="SELECT * FROM users")
-        return users[-1]
+        query = self.users.select().where(self.users.c.email == user["email"])
+        user = await self.db.fetch_one(query=query)
+        return User(**user)
 
-    async def update_user(self, user_id: int, data: dict) -> dict:
+    async def update_user(self, user_id: int, data: UserUpdate) -> User:
         if not await self.check_for_existing(user_id=user_id):
             raise HTTPException(status_code=404, detail="User not found")
-        query = """UPDATE users
-                   SET username = :username
-                   WHERE id = :id;
-                """
-        data["id"] = user_id
-        await self.db.execute(query, values=data)
-        query = "SELECT * FROM users WHERE id = :id"
-        user = await self.db.fetch_one(query, values={"id": user_id})
-        return user
 
-    async def delete_user(self, user_id):
+        query = self.users.update().where(self.users.c.id == user_id).values(username=data.username)
+        await self.db.execute(query)
+
+        query = self.users.select().where(self.users.c.id == user_id)
+        user = await self.db.fetch_one(query)
+        return User(**user)
+
+    async def delete_user(self, user_id) -> str:
         if not await self.check_for_existing(user_id=user_id):
             raise HTTPException(status_code=404, detail="User not found")
-        query = "DELETE FROM users WHERE id = :id"
-        await self.db.execute(query, values={"id": user_id})
+        query = self.users.delete().where(self.users.c.id == user_id)
+        await self.db.execute(query)
+        return "User deleted successfully"
